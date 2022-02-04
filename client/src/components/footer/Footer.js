5cef78db2a999aa07aea3751e4a1fb07c4dd39dc
Diego Ezequiel <diego.scardulla@outlook.com>
2022-02-04 18:37:05 -0300

    updated footer

--- client/src/components/footer/Footer.js
+++ client/src/components/footer/Footer.js
@@ -3,132 +3,31 @@ import "./footer.css";
 
 const Footer = () => {
   return (
-    <div className="app__box">
-      <h1 className="footer-title">Blog</h1>
-      <div className="container flex-center">
-        <div className="row">
-          <div className="column">
-            <h1 className="heading">About Us</h1>
-            <ul>
-              <li>
-                <a href="#" className="footer-link">
-                  <span>One</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="footer-link">
-                  <span>Two</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="footer-link">
-                  <span>Three</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="footer-link">
-                  <span>Four</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="column">
-            <h1 className="heading">Contact</h1>
-            <ul>
-              <li>
-                <a href="#" className="footer-link">
-                  <span>One</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="footer-link">
-                  <span>Two</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="footer-link">
-                  <span>Three</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="footer-link">
-                  <span>Four</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="column">
-            <h1 className="heading">FAQ</h1>
-            <ul>
-              <li>
-                <a href="#" className="footer-link">
-                  <span>One</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="footer-link">
-                  <span>Two</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="footer-link">
-                  <span>Three</span>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="footer-link">
-                  <span>Four</span>
-                </a>
-              </li>
-            </ul>
-          </div>
-          <div className="column">
-            <h1 className="heading">Social Media</h1>
-            <ul>
-              <li>
-                <a href="#" className="footer-link">
-                  <span
-                    className="iconify"
-                    data-icon="brandico:facebook-rect"
-                  ></span>
-                  <i>
-                    <span>Facebook</span>
-                  </i>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="footer-link">
-                  <span
-                    className="iconify"
-                    data-icon="brandico:twitter-bird"
-                  ></span>
-                  <i>
-                    <span>Twitter</span>
-                  </i>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="footer-link">
-                  <span
-                    className="iconify"
-                    data-icon="brandico:instagram"
-                  ></span>
-                  <i>
-                    <span>Instagram</span>
-                  </i>
-                </a>
-              </li>
-              <li>
-                <a href="#" className="footer-link">
-                  <span className="iconify" data-icon="brandico:github"></span>
-                  <i>
-                    <span>Github</span>
-                  </i>
-                </a>
-              </li>
-            </ul>
-          </div>
-        </div>
+    <div className="footer__container">
+      <div className="footer__copyright">
+        <small>Copyright © 2022 Diego & Diego</small>
+      </div>
+      <div className="footer__links">
+        <ul>
+          <li>
+            <span className="iconify" data-icon="brandico:facebook"></span>
+            <a href="#" className="footer__list-link">
+              Facebook
+            </a>
+          </li>
+          <li>
+            <span className="iconify" data-icon="brandico:github"></span>
+            <a href="#" className="footer__list-link">
+              GitHub
+            </a>
+          </li>
+          <li>
+            <span className="iconify" data-icon="brandico:linkedin"></span>
+            <a href="#" className="footer__list-link">
+              Linkedin
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
